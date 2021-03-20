f8094c54996d4106a69b760df967d74c7b416d64
Gaurav Bhardwaj <smartgaurav22@gmail.com>
2021-03-20 14:53:53 +0530

    Hello World

--- src/components/AdminContent/AdminContent.js
+++ src/components/AdminContent/AdminContent.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const AdminContent = () => {
+
     return (
         <div>
-            
+            <h1>Ye le *** ab kar dena approve</h1>        
         </div>
     )
 }
